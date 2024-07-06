18a5f1df216393b656f50edc2ef2184ad2562ac4
Gustianputra <putragustian212@gmail.com>
2024-07-07 05:29:02 +0700

    Update cpsettings.js

--- cpsetting/cpsettings.js
+++ cpsetting/cpsettings.js
@@ -2,23 +2,23 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 global.domain = "https://" // Isi Domain mu, WEBSITE WAJIB ADA SSL
-global.apikey = '-' // Isi Apikey Pterodactyl mu
-global.capikey = '-' // Isi Apikey Pterodactyl mu
-global.creAtor = "6281234824414@s.whatsapp.net"
-global.owner = ['6281234824414']
-global.ownerNumber = ["6281234824414@s.whatsapp.net"]
-global.nomerOwner = "6281234824414"
-global.namabotnya = 'CPanBot'
-global.namaownernya = 'CPanBot'
-global.packname = 'CPanBot'
+global.apikey = '-' //HRKU-27754ecf-0895-4563-bd5b-e07dca6ad743 Pterodactyl mu
+global.capikey = '-' // HRKU-27754ecf-0895-4563-bd5b-e07dca6ad743 Pterodactyl mu
+global.creAtor = "6283862145761@s.whatsapp.net"
+global.owner = ['6283862145761']
+global.ownerNumber = ["6283862145761@s.whatsapp.net"]
+global.nomerOwner = "6283861040903"
+global.namabotnya = 'Yanzz-Najwaa'
+global.namaownernya = 'Yanzz-Najwaa'
+global.packname = 'Yanzz-Najwaa'
 global.author = 'www.cpancloud.com'
 global.sessionName = 'session'
 global.email = 'admin@cpancloud.com' // Ganti dengan emailmu
-global.group = 'https://chat.whatsapp.com/'
+global.group = 'https://chat.whatsapp.com/GyiuqzWNZ0p1th2LjxXNTw'
 global.youtube = 'https://youtube.com/'
 global.website = 'https://www.cpancloud.com'
 global.github = 'https://github.com/Pann09'
-global.nomorowner = 'https://wa.me/6281224172387'
+global.nomorowner = 'https://wa.me/6283862145761'
 global.region = 'Indonesia'
 global.prefa = ['','!','.','#','-','•']
 global.krmd = 
@@ -30,7 +30,7 @@ owner: '```Owner Only Broo...```',
 group: '```Fitur Digunakan Hanya Untuk Group!!!```',
 private: '```Fitur Digunakan Hanya Untuk Private Chat!!!```',
 bot: '```Fitur Khusus Pengguna Nomor Bot!!!```',
-error: '```Error Kak, Hubungi owner 6281234824414```',
+error: '```Error Kak, Hubungi owner 6283862145761```',
 wait: '```Waittt...```'
 }
 
